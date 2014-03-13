e9df6cb0844fedb99bc0b93030492e591d978848
dangerousanimal <hideaki_ishii1204@yahoo.co.jp>
2014-03-14 08:43:56 +0900

    Practice

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,8 +7,7 @@ describe 'User pages' do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
-    it { should have_content(user.name) }
-    # it { should have_content(user.email) }
+    it { should have_title(user.name) }
   end
 
   describe 'signup page' do
@@ -22,18 +21,34 @@ describe 'User pages' do
       it 'should not create a user' do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe 'after submission' do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+       end
     end
 
     describe 'with valid information' do
       before do
         fill_in 'Name', with: 'Example User'
-        fill_in 'Email', with: 'use@example.com'
+        fill_in 'Email', with: 'user@example.com'
         fill_in 'Password', with: 'foobar'
         fill_in 'Confirmation', with: 'foobar'
       end
 
       it 'should create a user' do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe 'after saving a user' do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success') }
+        it { should have_content('Welcome to the sample app!') }
       end
     end
   end
